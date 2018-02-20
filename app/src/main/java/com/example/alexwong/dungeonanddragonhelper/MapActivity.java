6a07bef88dda987a03a8f7aa38d8513b94e79537
Alex Wong <alexwong11611@gmail.com>
2018-02-20 14:11:59 +0000

    Eight commit: Added functionality for google firebase saving and downloading of maps.

--- app/src/main/java/com/example/alexwong/dungeonanddragonhelper/MapActivity.java
+++ app/src/main/java/com/example/alexwong/dungeonanddragonhelper/MapActivity.java
@@ -1,17 +1,28 @@
 package com.example.alexwong.dungeonanddragonhelper;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
 import com.github.chrisbanes.photoview.PhotoView;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -19,11 +30,23 @@ import java.util.Random;
 
 public class MapActivity extends AppCompatActivity {
 
+    private static Context context;
+
     public static final int STORAGE_PERMISSIONS_REQUEST = 1;
     PhotoView photoView;
     Button mapButton;
     Button saveMapButton;
     Random r;
+
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+
+    StorageReference storageRef = storage.getReference();
+    StorageReference mapRef = storageRef.child("map.jpg");
+    StorageReference mapImageRef = storageRef.child("MapImages/map.jpg");
+    StorageReference storageReference = storage.getReference();
+    StorageReference pathReference = storageReference.child("MapImages/map.jpg");
+    StorageReference gsReference = storage.getReferenceFromUrl("gs://dungeonanddragonhelper.appspot.com/map.jpg");
+
 
     Integer[] images = {
             R.drawable.map1,
@@ -31,6 +54,7 @@ public class MapActivity extends AppCompatActivity {
             R.drawable.map3,
             R.drawable.map4
     };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +75,9 @@ public class MapActivity extends AppCompatActivity {
             }
         });
 
+        mapRef.getName().equals(mapImageRef.getName());
+        mapRef.getPath().equals(mapImageRef.getPath());
+
     }
 
     public void goHome(View view){
@@ -65,9 +92,9 @@ public class MapActivity extends AppCompatActivity {
 
         FileOutputStream outStream = null;
         File sdCard = Environment.getExternalStorageDirectory();
-        File dir = new File(sdCard.getAbsolutePath() + "/YourFolderName");
+        File dir = new File(sdCard.getAbsolutePath() + "/MapImages");
         dir.mkdirs();
-        String fileName = String.format("%d.jpg", System.currentTimeMillis());
+        String fileName = String.format("map.jpg");
         File outFile = new File(dir, fileName);
         outStream = new FileOutputStream(outFile);
         bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outStream);
@@ -76,7 +103,7 @@ public class MapActivity extends AppCompatActivity {
         success = true;
 
         if (success) {
-            Toast.makeText(getApplicationContext(), "Image saved with success to folder YourFolderName with name :" +fileName,
+            Toast.makeText(getApplicationContext(), "Image saved with success to folder MapImages with name :" +fileName,
                     Toast.LENGTH_LONG).show();
         } else {
             Toast.makeText(getApplicationContext(),
@@ -84,6 +111,39 @@ public class MapActivity extends AppCompatActivity {
         }
 
     }
+
+    public void putBytes(View view){
+        photoView.setDrawingCacheEnabled(true);
+        photoView.buildDrawingCache();
+        Bitmap bitmap = photoView.getDrawingCache();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] data = baos.toByteArray();
+
+        UploadTask uploadTask = mapRef.putBytes(data);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                Uri downloadUrl = taskSnapshot.getDownloadUrl();
+            }
+        });
+    }
+
+    public void getMapFromStorage(View view){
+        StorageReference gsReference = storage.getReferenceFromUrl("gs://dungeonanddragonhelper.appspot.com/map.jpg");
+        PhotoView photoView = findViewById(R.id.mapView);
+        Glide.with(this)
+                .using(new FirebaseImageLoader())
+                .load(gsReference)
+                .into(photoView);
+    }
+
 }
 
 
