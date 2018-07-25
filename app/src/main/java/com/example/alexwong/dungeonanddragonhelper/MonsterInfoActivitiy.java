2f941a7f6dc63102038da6f664794606b6cfd4dd
Alex Wong <alexwong11611@gmail.com>
2018-07-25 14:14:24 +0100

    Seventeenth commit: Added full functionality for monster index for Kobold and Goblin. Added pictures for bandits and wolves.

--- app/src/main/java/com/example/alexwong/dungeonanddragonhelper/MonsterInfoActivitiy.java
+++ app/src/main/java/com/example/alexwong/dungeonanddragonhelper/MonsterInfoActivitiy.java
@@ -12,6 +12,10 @@ public class MonsterInfoActivitiy extends AppCompatActivity {
 
     TextView gameInfo;
     PhotoView photoView;
+    TextView traitInfo;
+    TextView attackInfo;
+    TextView armorInfo;
+    TextView statsInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +24,22 @@ public class MonsterInfoActivitiy extends AppCompatActivity {
 
         gameInfo = findViewById(R.id.gameText);
         photoView = (PhotoView) findViewById(R.id.monsterView);
+        traitInfo = findViewById(R.id.traitsText);
+        attackInfo = findViewById(R.id.attacksText);
+        armorInfo = findViewById(R.id.armorText);
+        statsInfo = findViewById(R.id.statsText);
 
         String title = getIntent().getExtras().getString("TITLE");
         getSupportActionBar().setTitle(title);
-        getContentBasedOnTitle(title);
+        getFlavourTextBasedOnTitle(title);
+        getTraitTextBasedOnTitle(title);
+        getAttacksBasedOnTitle(title);
+        getArmorBasedOnTitle(title);
+        getStatsBasedOnTitle(title);
         getPictureBasedOnTitle(title);
-
     }
 
-    public void getContentBasedOnTitle(String title) {
+    public void getFlavourTextBasedOnTitle(String title) {
         String textToShow;
         switch (title) {
             case "GOBLIN\n":  textToShow = getString(R.string.goblin);
@@ -37,7 +48,7 @@ public class MonsterInfoActivitiy extends AppCompatActivity {
                 break;
             case "WOLF\n":  textToShow = getString(R.string.wolves);
                 break;
-            case "BANDITS":  textToShow = getString(R.string.bandits);
+            case "BANDIT\n":  textToShow = getString(R.string.bandits);
                 break;
             default: textToShow = "Invalid title";
                 break;
@@ -46,12 +57,59 @@ public class MonsterInfoActivitiy extends AppCompatActivity {
         gameInfo.setText(textToShow);
     }
 
-    public void getPictureBasedOnTitle(String title){
+    public void getTraitTextBasedOnTitle(String title){
+        String traitToShow = "";
+        switch (title){
+            case "GOBLIN\n": traitToShow = getString(R.string.goblinTrait);
+                break;
+            case "KOBOLD\n": traitToShow = getString(R.string.koboldTrait);
+                break;
+        }
+        traitInfo.setText(traitToShow);
+    }
 
+    public void getAttacksBasedOnTitle(String title){
+        String attackToShow = "";
+        switch(title){
+            case "GOBLIN\n": attackToShow = getString(R.string.goblinAttacks);
+                break;
+            case "KOBOLD\n": attackToShow = getString(R.string.koboldAttacks);
+                break;
+        }
+        attackInfo.setText(attackToShow);
+    }
+
+    public void getArmorBasedOnTitle(String title){
+        String armorToShow = "";
+        switch(title){
+            case "GOBLIN\n": armorToShow = getString(R.string.goblinArmor);
+                break;
+            case "KOBOLD\n": armorToShow = getString(R.string.koboldArmor);
+                break;
+        }
+        armorInfo.setText(armorToShow);
+    }
+
+    public void getStatsBasedOnTitle(String title){
+        String statsToShow = "";
+        switch(title){
+            case "GOBLIN\n": statsToShow = getString(R.string.goblinStats);
+                break;
+            case "KOBOLD\n": statsToShow = getString(R.string.koboldStats);
+                break;
+        }
+        statsInfo.setText(statsToShow);
+    }
+
+    public void getPictureBasedOnTitle(String title){
         switch (title){
             case "GOBLIN\n": photoView.setImageResource(R.drawable.goblin);
                 break;
             case "KOBOLD\n": photoView.setImageResource(R.drawable.kobold);
+                break;
+            case "WOLF\n": photoView.setImageResource(R.drawable.wolves);
+                break;
+            case "BANDIT\n": photoView.setImageResource(R.drawable.bandit);
                 break;
         }
     }
@@ -60,6 +118,11 @@ public class MonsterInfoActivitiy extends AppCompatActivity {
 
     public void goToMonDetection(View view) {
         Intent intent = new Intent(this, MonsterDetectionActivity.class);
+        startActivity(intent);
+    }
+
+    public void returnHome(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
